46073904f438eee7fee8f5c980d6a5f40f6da4a9
Mayu <Mayu@DESKTOP-GRFN1CA>
2024-09-26 20:02:20 +0100

    opentel

---
+++ src/main/java/com/mayu/my_spring_boot_app/HelloController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mayu.my_spring_boot_app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String hello() {
+        logger.info("Hello endpoint accessed"); // Log message
+        return "Hello, World!";
+    }
+}
